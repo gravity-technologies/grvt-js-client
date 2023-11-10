d043e70289e1313604daabb93239a9cf7b0f7f0a
danhlt <danh.danh20051995@gmail.com>
2023-11-10 17:11:34 +0700

    edit: schemaMap fn

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,21 +4,20 @@ export class Utils {
   /**
    * Maps a payload from a lite schema to a full schema or vice versa.
    */
-  static schemaMap (payload: any = {}, schemaMaps: Record<string, string> = {}) {
+  static schemaMap (payload: any = {}, schemaMaps: Record<string, string> = {}): any {
+    if (typeof payload !== 'object') {
+      return payload
+    }
+
+    if (Array.isArray(payload)) {
+      return payload.map((item) => Utils.schemaMap(item, schemaMaps))
+    }
+
     const result: Record<string, any> = {}
 
     for (const [key, value] of Object.entries(payload)) {
       const _key = schemaMaps[key] || key
-
-      if (typeof value === 'object') {
-        if (Array.isArray(value)) {
-          result[_key] = value.map((item) => Utils.schemaMap(item, schemaMaps))
-        } else {
-          result[_key] = Utils.schemaMap(value, schemaMaps)
-        }
-      } else {
-        result[_key] = value
-      }
+      result[_key] = Utils.schemaMap(value, schemaMaps)
     }
 
     return result
