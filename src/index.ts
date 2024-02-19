b086b233e422159674c3ee46bc9c2bd9c04af693
danh20051995 <danh.danh20051995@gmail.com>
2024-02-19 16:50:33 +0700

    merge TDG methods

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import { MDG } from './MDG'
-import {
-  TDG,
-  TDGAccount,
-  TDGAxe,
-  TDGOrder,
-  TDGRfq
-} from './TDG'
+import { TDG } from './TDG'
 export * from './MDG'
 export * from './TDG'
 export * from './interfaces'
 
 export default {
   MDG,
-
-  TDG,
-  TDGAccount,
-  TDGAxe,
-  TDGOrder,
-  TDGRfq
+  TDG
 }
