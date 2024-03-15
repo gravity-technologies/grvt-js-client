4d4b0d1fc5d0bc2125e7cf9adc7a88f9c4e752b5
loinguyen <loi@gravitygroup.io>
2024-03-15 12:17:31 +0700

    Revert "remove hex string map (#50)"
    This reverts commit 2bcf6625353c17f7eb03fabb11e79152400ed34f.

---
+++ src/interfaces/codegen/hex-string-map.ts
@@ -0,0 +1,13 @@
+// HexStringMap contains all bigints the should be encoded/decoded to/from hex strings
+export const HexStringMap = [
+  'from_account_id',
+  'from_eth_address',
+  'order_id',
+  'quote_id',
+  'r',
+  'rfq_id',
+  's',
+  'signer',
+  'to_account_id',
+  'to_eth_address'
+]
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { type SchemaMap } from '../interfaces'
+import { HexStringMap, type SchemaMap } from '../interfaces'
 
 export * from './json.utils'
 
@@ -19,7 +19,10 @@ export class Utils {
 
   static coverBigInt (field: string, value: any) {
     if (typeof value === 'bigint') {
-      return value.toString()
+      const hexStr = value.toString(16)
+      return HexStringMap.includes(field)
+        ? `0x${hexStr.length % 2 === 0 ? '' : '0'}${hexStr}`
+        : value.toString()
     }
     return value
   }
