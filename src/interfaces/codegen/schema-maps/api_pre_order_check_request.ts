bf5609da9c974be32d55ee6caef0a5bfc01720bb
Loi Nguyen <133964137+loinguyen0726@users.noreply.github.com>
2024-12-01 21:42:51 +0700

    update pre order check schema (#107)

--- src/interfaces/codegen/schema-maps/api_pre_order_check_request.ts
+++ src/interfaces/codegen/schema-maps/api_pre_order_check_request.ts
@@ -1,12 +1,14 @@
-import { type SchemaPairMap } from './types'
 import { ORDER_MAP } from './order'
+import { type SchemaPairMap } from './types'
 
 // Schema map for the 'API_PRE_ORDER_CHECK_REQUEST' struct.
 export const API_PRE_ORDER_CHECK_REQUEST_MAP: SchemaPairMap = Object.freeze({
   FULL_TO_LITE: {
-    order: ['o', ORDER_MAP.FULL_TO_LITE]
+    sub_account_id: 'sa',
+    orders: ['o', [ORDER_MAP.FULL_TO_LITE]]
   },
   LITE_TO_FULL: {
-    o: ['order', ORDER_MAP.LITE_TO_FULL]
+    sa: 'sub_account_id',
+    o: ['orders', [ORDER_MAP.LITE_TO_FULL]]
   }
 })
--- src/interfaces/codegen/schema-maps/api_pre_order_check_response.ts
+++ src/interfaces/codegen/schema-maps/api_pre_order_check_response.ts
@@ -1,20 +1,12 @@
+import { PRE_ORDER_CHECK_RESULT_MAP } from './pre_order_check_result'
 import { type SchemaPairMap } from './types'
-import { ASSET_MAX_QTY_MAP } from './asset_max_qty'
 
 // Schema map for the 'API_PRE_ORDER_CHECK_RESPONSE' struct.
 export const API_PRE_ORDER_CHECK_RESPONSE_MAP: SchemaPairMap = Object.freeze({
   FULL_TO_LITE: {
-    max_qty: ['mq', [ASSET_MAX_QTY_MAP.FULL_TO_LITE]],
-    margin_required: 'mr',
-    order_valid: 'ov',
-    reason: 'r',
-    settle_currency: 'sc'
+    results: ['r', [PRE_ORDER_CHECK_RESULT_MAP.FULL_TO_LITE]]
   },
   LITE_TO_FULL: {
-    mq: ['max_qty', [ASSET_MAX_QTY_MAP.LITE_TO_FULL]],
-    mr: 'margin_required',
-    ov: 'order_valid',
-    r: 'reason',
-    sc: 'settle_currency'
+    r: ['results', [PRE_ORDER_CHECK_RESULT_MAP.LITE_TO_FULL]]
   }
 })
