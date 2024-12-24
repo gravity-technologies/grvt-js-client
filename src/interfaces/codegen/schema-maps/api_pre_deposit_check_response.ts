e7c6213c7ce70613734530f9512d302a38b24ef2
Lê Thành Danh <danh.danh20051995@gmail.com>
2024-12-24 10:48:09 +0700

    [v1.3.13]: update pre deposit interface (#118)
    * [v1.3.13]: update pre deposit interface
    
    * patch version

--- src/interfaces/codegen/schema-maps/api_pre_deposit_check_response.ts
+++ src/interfaces/codegen/schema-maps/api_pre_deposit_check_response.ts
@@ -3,9 +3,11 @@ import { type SchemaPairMap } from './types'
 // Schema map for the 'API_PRE_DEPOSIT_CHECK_RESPONSE' struct.
 export const API_PRE_DEPOSIT_CHECK_RESPONSE_MAP: SchemaPairMap = Object.freeze({
   FULL_TO_LITE: {
-    max_deposit_limit: 'md'
+    max_deposit_limit: 'md',
+    currency: 'c'
   },
   LITE_TO_FULL: {
-    md: 'max_deposit_limit'
+    md: 'max_deposit_limit',
+    c: 'currency'
   }
 })
