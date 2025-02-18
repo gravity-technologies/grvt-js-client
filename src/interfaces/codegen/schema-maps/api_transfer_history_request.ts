901b09d2ce7aef45335362e28fbaa4fc2cfdcaa3
danhlt <danh.danh20051995@gmail.com>
2025-02-18 10:22:37 +0700

    [v1.3.27]: update Transfer interface

--- src/interfaces/codegen/schema-maps/api_transfer_history_request.ts
+++ src/interfaces/codegen/schema-maps/api_transfer_history_request.ts
@@ -7,13 +7,15 @@ export const API_TRANSFER_HISTORY_REQUEST_MAP: SchemaPairMap = Object.freeze({
     start_time: 'st',
     end_time: 'et',
     limit: 'l',
-    cursor: 'c1'
+    cursor: 'c1',
+    tx_id: 'ti'
   },
   LITE_TO_FULL: {
     c: 'currency',
     st: 'start_time',
     et: 'end_time',
     l: 'limit',
-    c1: 'cursor'
+    c1: 'cursor',
+    ti: 'tx_id'
   }
 })
--- src/interfaces/codegen/schema-maps/ws_transfer_feed_data_v_1.ts
+++ src/interfaces/codegen/schema-maps/ws_transfer_feed_data_v_1.ts
@@ -1,5 +1,5 @@
 import { type SchemaPairMap } from './types'
-import { TRANSFER_MAP } from './transfer'
+import { TRANSFER_HISTORY_MAP } from './transfer_history'
 
 // Schema map for the 'WS_TRANSFER_FEED_DATA_V_1' struct.
 export const WS_TRANSFER_FEED_DATA_V_1_MAP: SchemaPairMap = Object.freeze({
@@ -7,14 +7,14 @@ export const WS_TRANSFER_FEED_DATA_V_1_MAP: SchemaPairMap = Object.freeze({
     stream: 's',
     selector: 's1',
     sequence_number: 'sn',
-    feed: ['f', TRANSFER_MAP.FULL_TO_LITE],
+    feed: ['f', TRANSFER_HISTORY_MAP.FULL_TO_LITE],
     prev_sequence_number: 'ps'
   },
   LITE_TO_FULL: {
     s: 'stream',
     s1: 'selector',
     sn: 'sequence_number',
-    f: ['feed', TRANSFER_MAP.LITE_TO_FULL],
+    f: ['feed', TRANSFER_HISTORY_MAP.LITE_TO_FULL],
     ps: 'prev_sequence_number'
   }
 })
