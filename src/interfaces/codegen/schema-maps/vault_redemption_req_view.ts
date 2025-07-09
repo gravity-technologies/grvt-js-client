8e8f12d1b4f4d85a1e1be603ed1ed6e66306063a
Lê Thành Danh <danh.danh20051995@gmail.com>
2025-07-09 15:41:57 +0700

    [v1.4.25]: Sync APIs interfaces (#172)

--- src/interfaces/codegen/schema-maps/vault_redemption_req_view.ts
+++ src/interfaces/codegen/schema-maps/vault_redemption_req_view.ts
@@ -6,12 +6,14 @@ export const VAULT_REDEMPTION_REQ_VIEW_MAP: SchemaPairMap = Object.freeze({
     request_time: 'rt',
     currency: 'c',
     num_lp_tokens: 'nl',
-    max_redemption_period_timestamp: 'mr'
+    max_redemption_period_timestamp: 'mr',
+    age_category: 'ac'
   },
   LITE_TO_FULL: {
     rt: 'request_time',
     c: 'currency',
     nl: 'num_lp_tokens',
-    mr: 'max_redemption_period_timestamp'
+    mr: 'max_redemption_period_timestamp',
+    ac: 'age_category'
   }
 })
