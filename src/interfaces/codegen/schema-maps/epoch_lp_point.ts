675ebeeb1501057b8c6007175b55cb429a8f0f0e
danhlt <danh.danh20051995@gmail.com>
2025-07-11 22:41:37 +0700

    [v1.4.27] Sync APIs interfaces

---
+++ src/interfaces/codegen/schema-maps/epoch_lp_point.ts
@@ -0,0 +1,19 @@
+import { type SchemaPairMap } from './types'
+
+// Schema map for the 'EPOCH_LP_POINT' struct.
+export const EPOCH_LP_POINT_MAP: SchemaPairMap = Object.freeze({
+  FULL_TO_LITE: {
+    epoch: 'e',
+    main_account_id: 'ma',
+    lp_asset: 'la',
+    liquidity_score: 'ls',
+    vault_liquidity_score: 'vl'
+  },
+  LITE_TO_FULL: {
+    e: 'epoch',
+    ma: 'main_account_id',
+    la: 'lp_asset',
+    ls: 'liquidity_score',
+    vl: 'vault_liquidity_score'
+  }
+})
