3a2aa091ddf8dcb5e6f68f94ea14238d6fe6bae8
loinguyen <loi@gravitygroup.io>
2025-12-09 21:16:16 +0700

    [1.6.15] Update API interfaces

--- src/interfaces/codegen/schema-maps/api_query_trading_performance_request.ts
+++ src/interfaces/codegen/schema-maps/api_query_trading_performance_request.ts
@@ -3,11 +3,9 @@ import { type SchemaPairMap } from './types'
 // Schema map for the 'API_QUERY_TRADING_PERFORMANCE_REQUEST' struct.
 export const API_QUERY_TRADING_PERFORMANCE_REQUEST_MAP: SchemaPairMap = Object.freeze({
   FULL_TO_LITE: {
-    sub_account_id: 'sa',
-    asset: 'a'
+    sub_account_id: 'sa'
   },
   LITE_TO_FULL: {
-    sa: 'sub_account_id',
-    a: 'asset'
+    sa: 'sub_account_id'
   }
 })
