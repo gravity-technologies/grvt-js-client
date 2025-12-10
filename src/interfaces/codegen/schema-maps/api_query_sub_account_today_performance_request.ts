214c998146f308b48b4d2e597777dde1d3deeaeb
loinguyen <loi@gravitygroup.io>
2025-12-10 15:40:04 +0700

    [1.6.16] Sub Account Today Performance API

---
+++ src/interfaces/codegen/schema-maps/api_query_sub_account_today_performance_request.ts
@@ -0,0 +1,11 @@
+import { type SchemaPairMap } from './types'
+
+// Schema map for the 'API_QUERY_SUB_ACCOUNT_TODAY_PERFORMANCE_REQUEST' struct.
+export const API_QUERY_SUB_ACCOUNT_TODAY_PERFORMANCE_REQUEST_MAP: SchemaPairMap = Object.freeze({
+  FULL_TO_LITE: {
+    sub_account_id: 'sa'
+  },
+  LITE_TO_FULL: {
+    sa: 'sub_account_id'
+  }
+})
