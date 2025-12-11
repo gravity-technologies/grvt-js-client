64f4150a9239dd38596799909b27a0e42cfb1d50
Lê Thành Danh <danh.danh20051995@gmail.com>
2025-12-11 15:06:24 +0700

    [v1.6.18] Add EPositionMarginType.ISOLATED (#209)

--- src/interfaces/codegen/enums/position-margin-type.ts
+++ src/interfaces/codegen/enums/position-margin-type.ts
@@ -1,8 +1,11 @@
 export enum EPositionMarginType {
+  // Isolated Margin Mode: each position is allocated a fixed amount of collateral
+  ISOLATED = 'ISOLATED',
   // Cross Margin Mode: uses all available funds in your account as collateral across all cross margin positions
   CROSS = 'CROSS',
 }
 
 export const EPositionMarginTypeInt: Record<EPositionMarginType, number> = Object.freeze({
+  [EPositionMarginType.ISOLATED]: 1,
   [EPositionMarginType.CROSS]: 2
 })
